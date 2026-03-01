9bee215264a8268c70530565151bc0ba105d3ca5
b4ich3v <yoanbaychev@gmail.com>
2026-03-01 22:37:19 +0200

    Added isolated validation methods

--- source/numeric_integration/numeric_integral.py
+++ source/numeric_integration/numeric_integral.py
@@ -7,12 +7,32 @@ from source.data_structures.function import Function
 
 class NumericIntegral(ABC):
     def __init__(self, input_function: Function, input_interval: Interval, input_x_coords: list[float], input_sub_intervals: int) -> None:
-        if input_sub_intervals < 0 or len(input_x_coords) == 0:
-            raise RuntimeError("Count of input subintervals must be greater than 0 and input_x_coords must have atleast one element")
         self._interval = input_interval
         self._function = input_function
         self._sub_intervals = input_sub_intervals
         self._nodes = input_x_coords
+        self._validate_sub_intervals()
+        self._validate_nodes()
+
+    def _validate_sub_intervals(self) -> None:
+        if self._sub_intervals <= 0:
+            raise RuntimeError("Count of input subintervals must be greater than 0")
+
+    def _validate_nodes(self) -> None:
+        nodes = self._nodes
+        sub_intervals = self._sub_intervals
+        if len(nodes) != sub_intervals + 1:
+            raise RuntimeError("input_x_coords must have exactly sub_intervals + 1 nodes")
+
+        left = self._interval.get_left_component()
+        right = self._interval.get_right_component()
+        if nodes[0] != left or nodes[-1] != right:
+            raise RuntimeError("Nodes must start/end at the interval boundaries")
+
+        expected_step = (right - left) / sub_intervals
+        for i in range(1, len(nodes)):
+            if nodes[i] - nodes[i - 1] != expected_step:
+                raise RuntimeError("Nodes must be uniformly spaced across the interval")
 
     @abstractmethod
     def integrate(self):
