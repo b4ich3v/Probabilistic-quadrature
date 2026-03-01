ad3f34e465478c86087b9f70edb16823c25d6ad9
b4ich3v <yoanbaychev@gmail.com>
2026-03-01 22:15:58 +0200

    Implemented NumericIntegral abstract class

---
+++ source/numeric_integration/numeric_integral.py
@@ -0,0 +1,19 @@
+from abc import abstractmethod
+from abc import ABC
+
+from source.data_structures.interval import Interval
+from source.data_structures.function import Function
+
+
+class NumericIntegral(ABC):
+    def __init__(self, input_function: Function, input_interval: Interval, input_x_coords: list[float], input_sub_intervals: int) -> None:
+        if input_sub_intervals < 0 or len(input_x_coords) == 0:
+            raise RuntimeError("Count of input subintervals must be greater than 0 and input_x_coords must have atleast one element")
+        self._interval = input_interval
+        self._function = input_function
+        self._sub_intervals = input_sub_intervals
+        self._nodes = input_x_coords
+
+    @abstractmethod
+    def integrate(self, interval: Interval):
+        raise RuntimeError("Not implemented yet")
