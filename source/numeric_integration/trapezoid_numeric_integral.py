1bea8d20d6f11060c6c5ae39a6df9b77c68c4c70
b4ich3v <yoanbaychev@gmail.com>
2026-03-01 22:25:11 +0200

    Implemented TrapezoidNumericIntegral class

---
+++ source/numeric_integration/trapezoid_numeric_integral.py
@@ -0,0 +1,14 @@
+from source.numeric_integration.numeric_integral import NumericIntegral
+from source.data_structures.interval import Interval
+
+
+class TrapezoidNumericIntegral(NumericIntegral):
+    def __init__(self, input_interval: Interval, input_x_coords: list[float], input_sub_intervals: int):
+        super().__init__(input_interval, input_x_coords, input_sub_intervals)
+
+    def integrate(self):
+        result = 0
+        for i in range(1, self._sub_intervals + 1):
+            result += (self._function(self._nodes[i-1]) + self._function(self._nodes[i]))
+        result *= (self._interval.width) / (2 * self._sub_intervals)
+        return result
