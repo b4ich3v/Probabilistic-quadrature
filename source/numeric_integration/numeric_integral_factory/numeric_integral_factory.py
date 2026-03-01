408f9f4c28f5acf730f44b3abbcf8b13d7a13bc6
b4ich3v <yoanbaychev@gmail.com>
2026-03-02 00:11:21 +0200

    Adapted the new weighted nodes numeric integrals to the numeric intergral factory

--- source/numeric_integration/numeric_integral_factory/numeric_integral_factory.py
+++ source/numeric_integration/numeric_integral_factory/numeric_integral_factory.py
@@ -1,9 +1,13 @@
 from source.numeric_integration.numeric_integration_pattern import NumericIntegrationPattern
 from source.numeric_integration.numeric_integral import NumericIntegral
-from source.numeric_integration.rectangle_numeric_integral import RectangleNumericIntegral
-from source.numeric_integration.trapezoid_numeric_integral import TrapezoidNumericIntegral
-from source.numeric_integration.simpson_numeric_intergral import SimpsonNumericIntegral
+from source.numeric_integration.uniform_grid_numeric_integrals.rectangle_numeric_integral import RectangleNumericIntegral
+from source.numeric_integration.uniform_grid_numeric_integrals.trapezoid_numeric_integral import TrapezoidNumericIntegral
+from source.numeric_integration.uniform_grid_numeric_integrals.simpson_numeric_intergral import SimpsonNumericIntegral
 from source.numeric_integration.monte_carlo_numeric_integral import MonteCarloNumericIntegral
+from source.numeric_integration.weighted_nodes_numeric_integrals.gauss_legendre_integral import GaussLegendreIntegral
+from source.numeric_integration.weighted_nodes_numeric_integrals.gauss_hermite_integral import GaussHermiteIntegral
+from source.numeric_integration.weighted_nodes_numeric_integrals.gauss_laguerre_integral import GaussLaguerreIntegral
+from source.numeric_integration.weighted_nodes_numeric_integrals.gauss_chebyshev_integral import GaussChebyshevIntegral
 from source.numeric_integration.numeric_integral_factory.numeric_integral_abstract_factory import NumericIntegralAbstractFactory
 
 
@@ -17,4 +21,12 @@ class NumericIntegralFactory(NumericIntegralAbstractFactory):
             return SimpsonNumericIntegral(*args, **kwargs)
         if creation_pattern == NumericIntegrationPattern.MONTE_CARLO:
             return MonteCarloNumericIntegral(*args, **kwargs)
+        if creation_pattern == NumericIntegrationPattern.LEGENDRE:
+            return GaussLegendreIntegral(*args, **kwargs)
+        if creation_pattern == NumericIntegrationPattern.HERMITE:
+            return GaussHermiteIntegral(*args, **kwargs)
+        if creation_pattern == NumericIntegrationPattern.LAGUERRE:
+            return GaussLaguerreIntegral(*args, **kwargs)
+        if creation_pattern == NumericIntegrationPattern.CHEBYSHEV:
+            return GaussChebyshevIntegral(*args, **kwargs)
         raise ValueError(f"Unsupported integration pattern: {creation_pattern}")
