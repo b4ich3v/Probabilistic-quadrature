457763b7691b6747ffeef4a49f7ee0fe990b6262
b4ich3v <yoanbaychev@gmail.com>
2026-03-02 00:00:09 +0200

    Created middle abstract class UniformGridNumericIntegral

---
+++ source/numeric_integration/weighted_nodes_numeric_integral.py
@@ -0,0 +1,12 @@
+
+from source.numeric_integration.numeric_integral import NumericIntegral
+from source.data_structures.interval import Interval
+from source.data_structures.function import Function
+
+
+class WeightedNodesNumericIntegral(NumericIntegral):
+    def __init__(self, input_function: Function, nodes: list[float], weights: list[float], input_interval: Interval) -> None:
+        if len(nodes) != len(weights):
+            raise RuntimeError("nodes and weights must have the same length")
+        super().__init__(input_function, input_interval, nodes, len(nodes) - 1, validate=False)
+        self._weights = weights
