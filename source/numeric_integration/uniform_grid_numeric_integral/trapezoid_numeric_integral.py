5900979f571d9cabcde2f4983f68dbcbfea41754
b4ich3v <yoanbaychev@gmail.com>
2026-03-02 00:02:03 +0200

    Redirected

---
+++ source/numeric_integration/uniform_grid_numeric_integral/trapezoid_numeric_integral.py
@@ -0,0 +1,16 @@
+from source.numeric_integration.numeric_integral import UniformGridNumericIntegral
+from source.data_structures.interval import Interval
+from source.data_structures.function import Function
+
+
+class TrapezoidNumericIntegral(UniformGridNumericIntegral):
+    def __init__(self, input_function: Function, input_interval: Interval, input_x_coords: list[float], input_sub_intervals: int):
+        super().__init__(input_function, input_interval, input_x_coords, input_sub_intervals)
+
+    def integrate(self) -> float:
+        total = 0.0
+        nodes = self._nodes
+        f = self._function
+        for i in range(self._sub_intervals):
+            total += f(nodes[i]) + f(nodes[i + 1])
+        return total * (self._interval.width / (2 * self._sub_intervals))
