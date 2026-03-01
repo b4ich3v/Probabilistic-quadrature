74a090d6ac64d3e82cb3a0de29c4fafa2caf5e2c
b4ich3v <yoanbaychev@gmail.com>
2026-03-01 22:39:26 +0200

    Updated

--- source/numeric_integration/rectangle_numeric_integral.py
+++ source/numeric_integration/rectangle_numeric_integral.py
@@ -1,14 +1,17 @@
 from source.numeric_integration.numeric_integral import NumericIntegral
 from source.data_structures.interval import Interval
+from source.data_structures.function import Function
 
 
 class RectangleNumericIntegral(NumericIntegral):
-    def __init__(self, input_interval: Interval, input_x_coords: list[float], input_sub_intervals: int):
-        super().__init__(input_interval, input_x_coords, input_sub_intervals)
+    def __init__(self, input_function: Function, input_interval: Interval, input_x_coords: list[float], input_sub_intervals: int):
+        super().__init__(input_function, input_interval, input_x_coords, input_sub_intervals)
 
     def integrate(self):
-        result = 0
-        for i in range(1, self._sub_intervals + 1):
-            result += self._function((self._nodes[i - 1] + self._nodes[i]) / 2)
-        result *= (self._interval.width) / self._sub_intervals
-        return result
+        total = 0.0
+        nodes = self._nodes
+        f = self._function
+        for i in range(self._sub_intervals):
+            mid = 0.5 * (nodes[i] + nodes[i + 1])
+            total += f(mid)
+        return total * (self._interval.width / self._sub_intervals)
--- source/numeric_integration/simpson_numeric_intergral.py
+++ source/numeric_integration/simpson_numeric_intergral.py
@@ -1,14 +1,17 @@
 from source.numeric_integration.numeric_integral import NumericIntegral
 from source.data_structures.interval import Interval
+from source.data_structures.function import Function
 
 
-class SimpsonNumericIntergral(NumericIntegral):
-    def __init__(self, input_interval: Interval, input_x_coords: list[float], input_sub_intervals: int):
-        super().__init__(input_interval, input_x_coords, input_sub_intervals)
+class SimpsonNumericIntegral(NumericIntegral):
+    def __init__(self, input_function: Function, input_interval: Interval, input_x_coords: list[float], input_sub_intervals: int):
+        super().__init__(input_function, input_interval, input_x_coords, input_sub_intervals)
 
     def integrate(self):
-        result = 0
-        for i in range(1, self._sub_intervals + 1):
-            res += (self._function(self._nodes[i-1]) + 4 * self._function((self._nodes[i - 1] + self._nodes[i])/2) + self._function(self._nodes[i]))
-        result *= (self._interval.width) / (6 * self._sub_intervals)
-        return result
+        total = 0.0
+        nodes = self._nodes
+        f = self._function
+        for i in range(self._sub_intervals):
+            mid = 0.5 * (nodes[i] + nodes[i + 1])
+            total += f(nodes[i]) + 4 * f(mid) + f(nodes[i + 1])
+        return total * (self._interval.width / (6 * self._sub_intervals))
