cc11e935f21d30a87759ec84c0e9544ee2de16da
b4ich3v <yoanbaychev@gmail.com>
2026-03-01 23:08:41 +0200

    Implemented abstract class Kernel

---
+++ source/kernels/kernel.py
@@ -0,0 +1,20 @@
+from abc import ABC, abstractmethod
+import numpy as np
+
+
+class Kernel(ABC):
+    def _squared_euclidean(self, X: np.ndarray, Y: np.ndarray) -> np.ndarray:
+        X = np.atleast_2d(X)
+        Y = np.atleast_2d(Y)
+        XX = np.sum(np.square(X), axis=1)[:, None]
+        YY = np.sum(np.square(Y), axis=1)[None, :]
+        cross = X @ Y.T
+        return XX + YY - 2.0 * cross
+
+    @abstractmethod
+    def __call__(self, X: np.ndarray, Y: np.ndarray) -> np.ndarray:
+        raise RuntimeError("Not implemented yet")
+
+    @abstractmethod
+    def diag(self, X: np.ndarray) -> np.ndarray:
+        raise RuntimeError("Not implemented yet")
