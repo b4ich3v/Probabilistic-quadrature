028cd07b65973c2c26b5048a6673085ef43dbf49
b4ich3v <yoanbaychev@gmail.com>
2026-03-01 23:28:26 +0200

    Implemented NumericIntegralFactory class (realization of the abstract interface from abstract factory)

---
+++ source/numeric_integration/numeric_integral_factory/numeric_integral_factory.py
@@ -0,0 +1,20 @@
+from source.numeric_integration.numeric_integral_factory.numeric_integral_abstract_factory import NumericIntegralAbstractFactory
+from source.numeric_integration.numeric_integration_pattern import NumericIntegrationPattern
+from source.numeric_integration.numeric_integral import NumericIntegral
+from source.numeric_integration.rectangle_numeric_integral import RectangleNumericIntegral
+from source.numeric_integration.trapezoid_numeric_integral import TrapezoidNumericIntegral
+from source.numeric_integration.simpson_numeric_intergral import SimpsonNumericIntegral
+from source.numeric_integration.monte_carlo_numeric_integral import MonteCarloNumericIntegral
+
+
+class NumericIntegralFactory(NumericIntegralAbstractFactory):
+    def create(self, creation_pattern: NumericIntegrationPattern, *args, **kwargs) -> NumericIntegral:
+        if creation_pattern == NumericIntegrationPattern.RECTANGLE:
+            return RectangleNumericIntegral(*args, **kwargs)
+        if creation_pattern == NumericIntegrationPattern.TRAPEZOID:
+            return TrapezoidNumericIntegral(*args, **kwargs)
+        if creation_pattern == NumericIntegrationPattern.SIMPSON:
+            return SimpsonNumericIntegral(*args, **kwargs)
+        if creation_pattern == NumericIntegrationPattern.MONTE_CARLO:
+            return MonteCarloNumericIntegral(*args, **kwargs)
+        raise ValueError(f"Unsupported integration pattern: {creation_pattern}")
