5ee6fe9ef2cb2792fd3232952558ff6071da6547
b4ich3v <yoanbaychev@gmail.com>
2026-03-03 12:17:35 +0200

    Adapted the changes from monte carlo new dir

--- source/numeric_integration/numeric_integral_factory/numeric_integral_abstract_factory.py
+++ source/numeric_integration/numeric_integral_factory/numeric_integral_abstract_factory.py
@@ -1,11 +1,13 @@
 from abc import ABC
+from typing import Optional
 from abc import abstractmethod
 
 from source.numeric_integration.numeric_integration_pattern import NumericIntegrationPattern
 from source.numeric_integration.numeric_integral import NumericIntegral
+from source.numeric_integration.monte_carlo.monte_carlo_stretegies import MonteCarloIntegrationStrategy
 
 
 class NumericIntegralAbstractFactory(ABC):
     @abstractmethod
-    def create(self, creation_pattern: NumericIntegrationPattern, *args, **kwargs) -> NumericIntegral:
+    def create(self, creation_pattern: NumericIntegrationPattern, monte_carlo_pattern: Optional[MonteCarloIntegrationStrategy], *args, **kwargs) -> NumericIntegral:
         raise RuntimeError("Not implemented yet")
--- source/numeric_integration/numeric_integral_factory/numeric_integral_factory.py
+++ source/numeric_integration/numeric_integral_factory/numeric_integral_factory.py
@@ -1,9 +1,12 @@
+from typing import Optional
+
 from source.numeric_integration.numeric_integration_pattern import NumericIntegrationPattern
+from source.numeric_integration.monte_carlo.monte_carlo_factrory.monte_carlo_factory import MonteCarloAbstractFactory
+from source.numeric_integration.monte_carlo.monte_carlo_stretegies import MonteCarloIntegrationStrategy
 from source.numeric_integration.numeric_integral import NumericIntegral
 from source.numeric_integration.uniform_grid_numeric_integrals.rectangle_numeric_integral import RectangleNumericIntegral
 from source.numeric_integration.uniform_grid_numeric_integrals.trapezoid_numeric_integral import TrapezoidNumericIntegral
 from source.numeric_integration.uniform_grid_numeric_integrals.simpson_numeric_intergral import SimpsonNumericIntegral
-from source.numeric_integration.monte_carlo_numeric_integral import MonteCarloNumericIntegral
 from source.numeric_integration.weighted_nodes_numeric_integrals.gauss_legendre_integral import GaussLegendreIntegral
 from source.numeric_integration.weighted_nodes_numeric_integrals.gauss_hermite_integral import GaussHermiteIntegral
 from source.numeric_integration.weighted_nodes_numeric_integrals.gauss_laguerre_integral import GaussLaguerreIntegral
@@ -12,21 +15,13 @@ from source.numeric_integration.numeric_integral_factory.numeric_integral_abstra
 
 
 class NumericIntegralFactory(NumericIntegralAbstractFactory):
-    def create(self, creation_pattern: NumericIntegrationPattern, *args, **kwargs) -> NumericIntegral:
-        if creation_pattern == NumericIntegrationPattern.RECTANGLE:
-            return RectangleNumericIntegral(*args, **kwargs)
-        if creation_pattern == NumericIntegrationPattern.TRAPEZOID:
-            return TrapezoidNumericIntegral(*args, **kwargs)
-        if creation_pattern == NumericIntegrationPattern.SIMPSON:
-            return SimpsonNumericIntegral(*args, **kwargs)
-        if creation_pattern == NumericIntegrationPattern.MONTE_CARLO:
-            return MonteCarloNumericIntegral(*args, **kwargs)
-        if creation_pattern == NumericIntegrationPattern.LEGENDRE:
-            return GaussLegendreIntegral(*args, **kwargs)
-        if creation_pattern == NumericIntegrationPattern.HERMITE:
-            return GaussHermiteIntegral(*args, **kwargs)
-        if creation_pattern == NumericIntegrationPattern.LAGUERRE:
-            return GaussLaguerreIntegral(*args, **kwargs)
-        if creation_pattern == NumericIntegrationPattern.CHEBYSHEV:
-            return GaussChebyshevIntegral(*args, **kwargs)
+    def create(self, creation_pattern: NumericIntegrationPattern, monte_carlo_pattern: Optional[MonteCarloIntegrationStrategy], *args, **kwargs) -> NumericIntegral:
+        if creation_pattern == NumericIntegrationPattern.RECTANGLE: return RectangleNumericIntegral(*args, **kwargs)
+        if creation_pattern == NumericIntegrationPattern.TRAPEZOID: return TrapezoidNumericIntegral(*args, **kwargs)
+        if creation_pattern == NumericIntegrationPattern.SIMPSON: return SimpsonNumericIntegral(*args, **kwargs)
+        if creation_pattern == NumericIntegrationPattern.MONTE_CARLO: return MonteCarloAbstractFactory().create(monte_carlo_pattern)
+        if creation_pattern == NumericIntegrationPattern.LEGENDRE: return GaussLegendreIntegral(*args, **kwargs)
+        if creation_pattern == NumericIntegrationPattern.HERMITE: return GaussHermiteIntegral(*args, **kwargs)
+        if creation_pattern == NumericIntegrationPattern.LAGUERRE: return GaussLaguerreIntegral(*args, **kwargs)
+        if creation_pattern == NumericIntegrationPattern.CHEBYSHEV: return GaussChebyshevIntegral(*args, **kwargs)
         raise ValueError(f"Unsupported integration pattern: {creation_pattern}")
