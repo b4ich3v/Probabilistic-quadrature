dcb3d89019fd17acd186fc5ce0c564d368994ef6
b4ich3v <yoanbaychev@gmail.com>
2026-03-03 12:15:38 +0200

    Shift down by one

--- source/numeric_integration/numeric_integration_pattern.py
+++ source/numeric_integration/numeric_integration_pattern.py
@@ -2,11 +2,11 @@ from enum import Enum
 
 
 class NumericIntegrationPattern(Enum):
-    RECTANGLE = 1
-    TRAPEZOID = 2
-    SIMPSON = 3
-    MONTE_CARLO = 4
-    LEGENDRE = 5
-    HERMITE = 6
-    LAGUERRE = 7
-    CHEBYSHEV = 8
+    RECTANGLE = 0
+    TRAPEZOID = 1
+    SIMPSON = 2
+    MONTE_CARLO = 3
+    LEGENDRE = 4
+    HERMITE = 5
+    LAGUERRE = 6
+    CHEBYSHEV = 7
