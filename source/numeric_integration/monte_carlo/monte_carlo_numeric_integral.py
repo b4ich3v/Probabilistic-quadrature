457ec04b38859cd2a8ecdb74bd1f5fbbdc45e568
b4ich3v <yoanbaychev@gmail.com>
2026-03-06 23:38:42 +0200

    Added validation in constructor

--- source/numeric_integration/monte_carlo/monte_carlo_numeric_integral.py
+++ source/numeric_integration/monte_carlo/monte_carlo_numeric_integral.py
@@ -9,6 +9,8 @@ from source.random_variables.random_variable import RandomVariable
 
 class MonteCarloNumericIntegral(NumericIntegral):
     def __init__(self, input_function, measure: Measure, n_samples: int, rv: Optional[RandomVariable] = None, rng=None) -> None:
+        if n_samples <= 0:
+            raise ValueError("n_samples must be positive")
         super().__init__(input_function, Interval(0.0, 1.0), [0.0, 1.0], 1, validate=False)
         self._measure = measure
         self._n_samples = n_samples
