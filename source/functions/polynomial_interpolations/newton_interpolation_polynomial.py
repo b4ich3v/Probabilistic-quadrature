0f90670bd871b4c6a204b882765f3831d6cbe068
b4ich3v <yoanbaychev@gmail.com>
2026-03-09 17:34:46 +0200

    Updated

--- source/functions/polynomial_interpolations/newton_interpolation_polynomial.py
+++ source/functions/polynomial_interpolations/newton_interpolation_polynomial.py
@@ -4,19 +4,22 @@ from source.functions.polynomial_interpolations.interpolation_polynomial import 
 class NewtonInterpolationPoly(InterpolationPoly):
     def __init__(self, nodes: list[float], values: list[float]) -> None:
         super().__init__(nodes, values)
+        self._coefficients = self._compute_divided_differences()
 
-    def _diff(self):
-        if len(self._nodes) == 1:
-            return self._values[0]
-        else:
-            return (self._diff(self._nodes[1:], self._values[1:]) - 
-                    self._diff(self._nodes[:-1], self._values[:-1])) / (
-                    self._nodes[-1] - self._nodes[0])
+    def _compute_divided_differences(self) -> list[float]:
+        n = len(self._nodes)
+        coeffs = list(self._values)
+        for order in range(1, n):
+            for i in range(n - 1, order - 1, -1):
+                numerator = coeffs[i] - coeffs[i - 1]
+                denominator = self._nodes[i] - self._nodes[i - order]
+                coeffs[i] = numerator / denominator
+        return coeffs
 
     def evaluate(self, x: float) -> float:
-            result = 0
-            product = 1
-            for i in range(len(self._nodes)):
-                result += self._diff(self._nodes[:i+1], self._values[:i+1]) * product
-                product *= (x - self._nodes[i])
-            return result
+        if not self._coefficients:
+            return 0.0
+        result = self._coefficients[-1]
+        for i in range(len(self._nodes) - 2, -1, -1):
+            result = self._coefficients[i] + (x - self._nodes[i]) * result
+        return result
