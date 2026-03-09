16f401f833831896af87ecc2d5e150808a772b8b
b4ich3v <yoanbaychev@gmail.com>
2026-03-09 17:15:55 +0200

    Implemented abstract + non abstract factory classes for creating different polynomial interpolations by criteria (pattern)

---
+++ source/functions/polynomial_interpolations/polynomial_interpolation_factory/polynomial_interpolation_factory.py
@@ -0,0 +1,15 @@
+from source.functions.polynomial_interpolations.interpolation_polynomial import InterpolationPoly
+from source.functions.polynomial_interpolations.interpolation_pattern import InterpolationPattern
+from source.functions.polynomial_interpolations.polynomial_interpolation_factory.polynomial_interpolation_abstract_factory import PolyInterpolationAbstractFactory
+from source.functions.polynomial_interpolations.lagrange_interpolation_polynomial import LagrangeInterpolationPoly
+from source.functions.polynomial_interpolations.hermit_interpolation_polynomial import HermitInterpolationPoly
+from source.functions.polynomial_interpolations.lagrange_interpolation_polynomial import LagrangeInterpolationPoly
+from source.functions.polynomial_interpolations.newton_interpolation_polynomial import NewtonInterpolationPoly
+
+
+class PolynomialInterpolationFactory(PolyInterpolationAbstractFactory):
+    def create(self, creation_pattern: InterpolationPattern, nodes: list[float], values: list[float]) -> InterpolationPoly:
+        if creation_pattern == InterpolationPattern.LAGRANGE: return LagrangeInterpolationPoly(nodes, values)
+        if creation_pattern == InterpolationPattern.HERMIT: return HermitInterpolationPoly(nodes, values)
+        if creation_pattern == InterpolationPattern.NEWTON: return NewtonInterpolationPoly(nodes, values)
+        raise ValueError(f"Unsupported polynomial interpolation pattern: {creation_pattern}")
