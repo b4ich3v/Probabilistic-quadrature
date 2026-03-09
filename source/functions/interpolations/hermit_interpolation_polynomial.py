b44fcf51489a92e09eb8300c979051bb78ceec32
b4ich3v <yoanbaychev@gmail.com>
2026-03-09 17:01:17 +0200

    Implemented HermitInterpolationPoly class

---
+++ source/functions/interpolations/hermit_interpolation_polynomial.py
@@ -0,0 +1,23 @@
+import math
+
+from source.functions.interpolations.interpolation_polynomial import InterpolationPoly
+
+
+class HermitInterpolationPoly(InterpolationPoly):
+    def __init__(self, nodes: list[float], values: list[float]) -> None:
+        super().__init__(nodes, values)
+
+    def _diff(self, left: int, right: int):
+        if self._nodes[left] == self._nodes[right]: 
+            return self._values[left] / math.factorial(right - left)
+        return (self._diff(self._nodes, self._values, left + 1, right) - 
+                self._diff(self._nodes, self._values, left, right - 1)) / (
+                self._nodes[right] - self._nodes[left])
+
+    def evaluate(self, x: float) -> float:
+        result = 0
+        product = 1
+        for i in range(len(self._values)):
+            result += self._diff(self._nodes, self._values, 0, i) * product
+            product *= (x - self._nodes[i])
+        return result
