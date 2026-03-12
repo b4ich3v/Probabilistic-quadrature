a1395567792c63a9fd474bfc2fe6a81d29ff9f78
b4ich3v <yoanbaychev@gmail.com>
2026-03-12 22:13:20 +0200

    Add __init__.py with re-exports

---
+++ source/measures/__init__.py
@@ -0,0 +1,5 @@
+from source.measures.measure import Measure
+from source.measures.gaussian_measure import GaussianMeasure
+from source.measures.uniform_box_measure import UniformBoxMeasure
+
+__all__ = ["Measure", "GaussianMeasure", "UniformBoxMeasure"]
