8c4a0650c863fb4cf9dabe1a5c50b94ef74a412e
b4ich3v <yoanbaychev@gmail.com>
2026-03-12 21:55:33 +0200

    Add __init__.py with re-exports for clean imports

---
+++ source/functions/__init__.py
@@ -0,0 +1,7 @@
+from source.functions.interval import Interval
+from source.functions.domain import Domain
+from source.functions.range import Range
+from source.functions.function import Function
+from source.functions.measured_function import MeasuredFunction
+
+__all__ = ["Interval", "Domain", "Range", "Function", "MeasuredFunction"]
