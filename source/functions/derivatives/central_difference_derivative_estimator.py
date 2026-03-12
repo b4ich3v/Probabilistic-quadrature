b167b352347faa7bf0d2f0dfd0660206a2f93dca
b4ich3v <yoanbaychev@gmail.com>
2026-03-12 22:00:58 +0200

    Remove redundant __init__, inherit parent constructor

--- source/functions/derivatives/central_difference_derivative_estimator.py
+++ source/functions/derivatives/central_difference_derivative_estimator.py
@@ -1,10 +1,7 @@
 from source.functions.derivatives.derivative_estimator import DerivativeEstimator
-from source.functions.function import Function
 
 
 class CentralDifferenceDerivativeEstimator(DerivativeEstimator):
-    def __init__(self, function: Function, precision: float) -> None:
-        super().__init__(function, precision)
-
     def calculate_derivative_at(self, point: float) -> float:
-        return (self._function(point + self._precision) - self._function(point - self._precision)) / (2 * self._precision)
+        h = self._precision
+        return (self._function(point + h) - self._function(point - h)) / (2 * h)
