8a4deaf4b32e33614beb20258f3ed5b4c1c8f275
b4ich3v <yoanbaychev@gmail.com>
2026-03-12 22:25:10 +0200

    Fix filename typo (intergral->integral), eliminate double function evaluations, remove input_ prefix

---
+++ source/numeric_integration/uniform_grid_numeric_integrals/simpson_numeric_integral.py
@@ -0,0 +1,21 @@
+from source.numeric_integration.uniform_grid_numeric_integral import UniformGridNumericIntegral
+from source.functions.interval import Interval
+from source.functions.function import Function
+
+
+class SimpsonNumericIntegral(UniformGridNumericIntegral):
+    def __init__(self, func: Function, interval: Interval, nodes: list[float], sub_intervals: int):
+        super().__init__(func, interval, nodes, sub_intervals)
+
+    def integrate(self) -> float:
+        nodes = self._nodes
+        f = self._function
+        n = self._sub_intervals
+        f_values = [f(x) for x in nodes]
+        total = f_values[0] + f_values[-1]
+        for i in range(1, n):
+            total += 2.0 * f_values[i]
+        for i in range(n):
+            mid = 0.5 * (nodes[i] + nodes[i + 1])
+            total += 4.0 * f(mid)
+        return total * (self._interval.width / (6 * n))
