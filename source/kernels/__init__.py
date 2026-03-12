b94d58296a748715df17b77d23f84a1757c4de76
b4ich3v <yoanbaychev@gmail.com>
2026-03-12 22:10:47 +0200

    Add __init__.py with re-exports

---
+++ source/kernels/__init__.py
@@ -0,0 +1,5 @@
+from source.kernels.kernel import Kernel
+from source.kernels.rbf_kernel import RBFKernel
+from source.kernels.matern32_kernel import Matern32Kernel
+
+__all__ = ["Kernel", "RBFKernel", "Matern32Kernel"]
