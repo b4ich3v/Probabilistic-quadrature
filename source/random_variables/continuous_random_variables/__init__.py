c599ce1a3cba3fd0369533f58843f7100b84f306
b4ich3v <yoanbaychev@gmail.com>
2026-03-12 22:42:02 +0200

    Add __init__.py with re-exports

---
+++ source/random_variables/continuous_random_variables/__init__.py
@@ -0,0 +1,5 @@
+from source.random_variables.continuous_random_variables.uniform import Uniform
+from source.random_variables.continuous_random_variables.normal import Normal
+from source.random_variables.continuous_random_variables.uniform_box import ContinuousUniformBox
+
+__all__ = ["Uniform", "Normal", "ContinuousUniformBox"]
