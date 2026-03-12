92aa2700c0e47f8e58bcd00c2150e02d36f45784
b4ich3v <yoanbaychev@gmail.com>
2026-03-12 22:05:26 +0200

    Add __init__.py with re-exports

---
+++ source/functions/polynomial_interpolations/__init__.py
@@ -0,0 +1,10 @@
+from source.functions.polynomial_interpolations.interpolation_polynomial import InterpolationPoly
+from source.functions.polynomial_interpolations.interpolation_pattern import InterpolationPattern
+from source.functions.polynomial_interpolations.lagrange_interpolation_polynomial import LagrangeInterpolationPoly
+from source.functions.polynomial_interpolations.newton_interpolation_polynomial import NewtonInterpolationPoly
+from source.functions.polynomial_interpolations.hermite_interpolation_polynomial import HermiteInterpolationPoly
+
+__all__ = [
+    "InterpolationPoly", "InterpolationPattern",
+    "LagrangeInterpolationPoly", "NewtonInterpolationPoly", "HermiteInterpolationPoly",
+]
