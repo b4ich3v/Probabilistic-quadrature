5b967f1e931e3fa76e2549b872cc002f4b8789b5
b4ich3v <yoanbaychev@gmail.com>
2026-03-12 22:20:05 +0200

    Use Interval .left/.right properties instead of getters

--- source/numeric_integration/weighted_nodes_numeric_integrals/affine_transformation.py
+++ source/numeric_integration/weighted_nodes_numeric_integrals/affine_transformation.py
@@ -6,10 +6,10 @@ from source.functions.interval import Interval
 class AffineTransformation:
     @staticmethod
     def map_from_unit(nodes: np.ndarray, weights: np.ndarray, interval: Interval) -> tuple[np.ndarray, np.ndarray]:
-        a = interval.get_left_component()
-        b = interval.get_right_component()
+        a = interval.left
+        b = interval.right
         scale = 0.5 * (b - a)
         shift = 0.5 * (a + b)
-        mapped_nodes = scale * nodes + shift  # x = ((b - a) / 2) * t + (a + b) / 2
-        mapped_weights = scale * weights  # w' = ((b - a) / 2) * w
+        mapped_nodes = scale * nodes + shift
+        mapped_weights = scale * weights
         return mapped_nodes, mapped_weights
