eb0aea4a13eaa50c8dc2d2cc8808312f8854285e
b4ich3v <yoanbaychev@gmail.com>
2026-03-12 22:31:29 +0200

    Make proposal_sampler and weight_fn required (fail-fast), remove input_ prefix

--- source/numeric_integration/monte_carlo/weighted_monte_carlo_integral.py
+++ source/numeric_integration/monte_carlo/weighted_monte_carlo_integral.py
@@ -1,5 +1,5 @@
 import numpy as np
-from typing import Optional, Callable
+from typing import Callable
 
 from source.numeric_integration.monte_carlo.monte_carlo_numeric_integral import MonteCarloNumericIntegral
 from source.measures.measure import Measure
@@ -8,9 +8,9 @@ from source.random_variables.continuous_random_variables.uniform import Uniform
 
 
 class WeightedMonteCarloIntegral(MonteCarloNumericIntegral):
-    def __init__(self, input_function, measure: Measure, n_samples: int, rv: Optional[RandomVariable] = None,
-        proposal_sampler: Optional[Callable[[int, RandomVariable], np.ndarray]] = None, weight_fn: Optional[Callable[[np.ndarray], np.ndarray]] = None):
-        super().__init__(input_function, measure, n_samples, rv)
+    def __init__(self, func: Callable, measure: Measure, n_samples: int,
+        proposal_sampler: Callable[[int, RandomVariable], np.ndarray], weight_fn: Callable[[np.ndarray], np.ndarray], rv: RandomVariable | None = None):
+        super().__init__(func, measure, n_samples, rv)
         self._proposal_sampler = proposal_sampler
         self._weight_fn = weight_fn
 
@@ -18,9 +18,6 @@ class WeightedMonteCarloIntegral(MonteCarloNumericIntegral):
         return self._rv if self._rv is not None else Uniform(0.0, 1.0)
 
     def integrate(self) -> float:
-        if self._proposal_sampler is None or self._weight_fn is None:
-            raise NotImplementedError("Weighted MC: proposal_sampler/weight_fn are required")
-
         rv = self._default_rv()
         samples = self._proposal_sampler(self._n_samples, rv)
         weights = np.asarray(self._weight_fn(samples), dtype=float)
