11746867313d1c2a24853889dc20ea06631123db
b4ich3v <yoanbaychev@gmail.com>
2026-03-12 22:25:38 +0200

    Eliminate double function evaluations at shared nodes, remove input_ prefix

--- source/numeric_integration/uniform_grid_numeric_integrals/trapezoid_numeric_integral.py
+++ source/numeric_integration/uniform_grid_numeric_integrals/trapezoid_numeric_integral.py
@@ -4,13 +4,15 @@ from source.functions.function import Function
 
 
 class TrapezoidNumericIntegral(UniformGridNumericIntegral):
-    def __init__(self, input_function: Function, input_interval: Interval, input_x_coords: list[float], input_sub_intervals: int):
-        super().__init__(input_function, input_interval, input_x_coords, input_sub_intervals)
+    def __init__(self, func: Function, interval: Interval, nodes: list[float], sub_intervals: int):
+        super().__init__(func, interval, nodes, sub_intervals)
 
     def integrate(self) -> float:
-        total = 0.0
         nodes = self._nodes
         f = self._function
-        for i in range(self._sub_intervals):
-            total += f(nodes[i]) + f(nodes[i + 1])
-        return total * (self._interval.width / (2 * self._sub_intervals))
+        n = self._sub_intervals
+        f_values = [f(x) for x in nodes]
+        total = f_values[0] + f_values[-1]
+        for i in range(1, n):
+            total += 2.0 * f_values[i]
+        return total * (self._interval.width / (2 * n))
