59ee31ec86682420f446c8efbd05449a37ad6258
b4ich3v <yoanbaychev@gmail.com>
2026-03-12 22:41:33 +0200

    Use ellipsis in abstract bodies, type rng as Generator, rename _probability/calculate_probability to pdf

--- source/random_variables/random_variable.py
+++ source/random_variables/random_variable.py
@@ -5,27 +5,19 @@ import numpy as np
 class RandomVariable(ABC):
     @property
     @abstractmethod
-    def dim(self) -> int:
-        raise NotImplementedError
+    def dim(self) -> int: ...
 
     @abstractmethod
-    def sample(self, n: int = 1, rng=None) -> np.ndarray:
-        raise NotImplementedError
+    def sample(self, n: int = 1, rng: np.random.Generator | None = None) -> np.ndarray: ...
 
     @abstractmethod
-    def log_prob(self, x: np.ndarray) -> np.ndarray:
-        raise NotImplementedError
+    def log_prob(self, x: np.ndarray) -> np.ndarray: ...
 
     @abstractmethod
-    def mean(self) -> np.ndarray:
-        raise NotImplementedError
+    def mean(self) -> np.ndarray: ...
 
     @abstractmethod
-    def var(self) -> np.ndarray:
-        raise NotImplementedError
+    def var(self) -> np.ndarray: ...
 
-    def _probability(self, x: np.ndarray) -> np.ndarray:
+    def pdf(self, x: np.ndarray) -> np.ndarray:
         return np.exp(self.log_prob(x))
-
-    def calculate_probability(self, x: np.ndarray) -> np.ndarray:
-        return self._probability(x)
