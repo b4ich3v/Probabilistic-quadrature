48f998bd359337c8dcc53b4e4bfe641d6f207eba
b4ich3v <yoanbaychev@gmail.com>
2026-03-12 22:16:37 +0200

    Add __init__.py with re-exports

---
+++ source/numeric_integration/__init__.py
@@ -0,0 +1,4 @@
+from source.numeric_integration.numeric_integral import NumericIntegral
+from source.numeric_integration.numeric_integration_pattern import NumericIntegrationPattern
+
+__all__ = ["NumericIntegral", "NumericIntegrationPattern"]
