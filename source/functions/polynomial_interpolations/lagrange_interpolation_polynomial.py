9bd8e263c9df674088912d759573e543989246ab
b4ich3v <yoanbaychev@gmail.com>
2026-03-12 22:07:47 +0200

    Remove redundant __init__, fix result init to 0.0

--- source/functions/polynomial_interpolations/lagrange_interpolation_polynomial.py
+++ source/functions/polynomial_interpolations/lagrange_interpolation_polynomial.py
@@ -2,14 +2,12 @@ from source.functions.polynomial_interpolations.interpolation_polynomial import 
 
 
 class LagrangeInterpolationPoly(InterpolationPoly):
-    def __init__(self, nodes: list[float], values: list[float]) -> None:
-        super().__init__(nodes, values)
-
     def evaluate(self, x: float) -> float:
-        result = 0
-        for i in range(len(self._nodes)):
+        result = 0.0
+        n = len(self._nodes)
+        for i in range(n):
             term = self._values[i]
-            for j in range(len(self._nodes)):
+            for j in range(n):
                 if i != j:
                     term *= (x - self._nodes[j]) / (self._nodes[i] - self._nodes[j])
             result += term
